4fbeddef3643180d78770b0f71611ce640a835b6
Bill Jameson <bill.jameson.mail@gmail.com>
2025-01-14 15:28:55 -0500

    add questions

--- app/data.js
+++ app/data.js
@@ -1,7 +1,64 @@
 let interviewerQuestions = [
-    //General developer
+    /*** General Job */
     {
-        questionText: "What is Dependency Injection?"
+        questionText: "Why did you leave your last job?"
+        , answers: [
+            "The project came to an end."
+            , "I was having some issues going on in my personal life I wanted to work on."
+            , "I left on good terms - they will give me a very good recommendation."
+        ]
+    }
+
+    , {
+        questionText: "Are you willing to relocate?"
+        , answers: [
+            "Yes"
+            , "I am going through some life changes and have been thinking about changing my living environment."
+            , "I grew up in Pensacola, so I am familiar with that area of the country."
+            , "I would like to be closer to the Gulf of Mexico. I miss the ocean."
+            , "It's not that far from Atlanta. It's not like moving across the country."
+            , "If I decided to move back to Atlanta it wouldn't be a huge deal."
+        ]
+    }
+
+    , {
+        questionText: "What factors will you consider to take this job?"
+        , answers: [
+            "Salary and benefits - it's more than I have been making."
+            , "The location - the environment I will be living in."
+            , "The nature of the job."
+            , "Meeting the people I will be working with."
+            , "What kind of assistance will they be providing with the relocation."
+            , "Atmosphere - I like working in a friendly, relaxed environment."
+        ]
+    }
+
+    , {
+        questionText: "Do you have any questions for me? (recruiter)"
+        , answers: [
+            "Why do they require on-site?"
+            , "What kind of assistance will they be providing with the relocation?"
+            , "What is the benefits package like?"
+            , "How big is the company."
+            , "What are the facilities like?"
+            , "What is the location environment like? (city, rural, in-between)"
+            , "Anything you can tell me about the nature of the job."
+            , "When will the job start?"
+            , "What is your position in the company?"
+        ]
+    }
+
+    /*** Specific Job ***/
+    , {
+        questionText: "Questions for Sr. Developer - web/.net"
+        , answers: [
+            "web.net?"
+            , "Blazer?"
+        ]
+    }
+
+    /*** General Developer ***/
+    , {questionText: "What is Dependency Injection?"
         , answers: [
             "Promotes loose coupling"
             , "Object receives its dependencies from an external source rather than creating them"
@@ -9,7 +66,7 @@ let interviewerQuestions = [
         ]
     }
 
-    //React
+    /*** React ***/
     , {
         questionText: "What is React?"
         , answers: [
@@ -27,6 +84,86 @@ let interviewerQuestions = [
             "HTML-like language for React"
             , "Can combine with ordinary HTML"
             , "Component names are tag names"
+        ]
+    }
+
+    /*** ETL ***/
+    , {
+        questionText: "What does ETL stand for?"
+        , answers: [
+            "Extract"
+            , "Transform"
+            , "Load"
+        ]
+    }
+
+    , {
+        questionText: "What is ETL for?"
+        , answers: [
+            "Retrieve data from one or more source systems"
+            , "Apply operations to the source, such as cleaning, filtering, standardizing, or combining data from different sources. This step ensures the data is structured and prepared for its intended use."
+            , "Move the transformed data to the target system"
+        ]
+    }
+
+    , {
+        questionText: "What ETL operations are performed on the Transform step?"
+        , answers: [
+            "Cleaning"
+            , "Formatting"
+            , "Standardizing"
+            , "Filtering"
+            , "Combining"
+        ]
+    }
+
+    /*** SQL ***/
+
+    , {
+        questionText: "What does SQL stand for?"
+        , answers: [
+            "Structured Query Language"
+        ]
+    }
+
+    /*** Data Warehouse ***/
+
+    , {
+        questionText: "What is a data warehouse?"
+        , answers: [
+            "Centralized repository of large amounts of data"
+            , "The data can come from multiple sources"
+            , "Optimized for querying and analysis rather than transaction processing"
+        ]
+    }
+ 
+    /*** Blazor ***/
+
+    , {
+        questionText: "What is Blazor?"
+        , answers: [
+            "For building website front ends"
+            , "Creates single-page applications"
+            , "Uses C# and .NET instead of JavaScript"
+        ]
+    }
+
+    , {
+        questionText: "What are the two main hosing models for Blazor?"
+        , answers: [
+            "Blazer Server"
+            , "Blazor WebAssembly"
+        ]
+    }
+
+    /*** ASP.NET ***/
+
+    , {
+        questionText: "What is ASP.NET?"
+        , answers: [
+            "Microsoft platform for building web applications and services"
+            , "Uses C# or VB.NET"
+            , "Has cross-platform support"
         ]
     }
 ];
